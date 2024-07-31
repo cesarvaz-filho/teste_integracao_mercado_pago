3c7196984ee7c1b01b283f02daf62326ec338d05
César Augusto Vaz Sampaio Filho <62805432+cesarvaz-filho@users.noreply.github.com>
2024-07-31 08:58:54 -0300

    Delete src directory

--- src/routes/paymentRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const paymentController = require('../controllers/paymentController');
-
-router.post('/create_payment', paymentController.createPayment);
-
-module.exports = router;
